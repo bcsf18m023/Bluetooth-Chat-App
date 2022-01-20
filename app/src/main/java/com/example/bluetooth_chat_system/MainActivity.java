884da9361cca1d34e7100f1c968d75202d5d969b
Shahbaz <bcsf18m038@pucit.edu.pk>
2022-01-20 17:08:02 +0500

    Take Location Permissions from user

--- app/src/main/java/com/example/bluetooth_chat_system/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_chat_system/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.bluetooth_chat_system;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +19,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
+    private final int LOCATION_PERMISSION_REQUEST=101;
     private BluetoothAdapter bluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +71,43 @@ public class MainActivity extends AppCompatActivity {
         else
         {
             bluetoothAdapter.enable();
+        }
+    }
+    private void checkPermission()
+    {
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==LOCATION_PERMISSION_REQUEST)
+        {
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+            }else{
+                new AlertDialog.Builder(context)
+                    .setCancelable(false)
+                    .setMessage("Location permission is required.Plaese grant Permission.")
+                        .setPositiveButton("Garnt", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                checkPermission();
+                            }
+
+                        })
+                        .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .create();
+            }
+        }else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
