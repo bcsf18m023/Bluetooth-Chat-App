ba38331d2eb32142f3f0b3da2e506acffa857fc2
bcsf18m023 <bcsf18m023@pucit.edu.pk>
2022-01-20 08:52:18 +0500

    Home Screen: Menue Elememts are Arranged And Working

--- app/src/main/java/com/example/bluetooth_chat_system/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_chat_system/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.bluetooth_chat_system;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.menu_search_device:
+                Toast.makeText(context,"this is device search",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_bluetooth_on:
+                Toast.makeText(context,"this is bluetooth connection",Toast.LENGTH_SHORT).show();
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 }
