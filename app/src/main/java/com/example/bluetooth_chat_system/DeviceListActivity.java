a690ddf88a2df8930525999d1eab0c315308ae39
Shahbaz <bcsf18m038@pucit.edu.pk>
2022-01-20 17:48:10 +0500

    Paired Devices list added

--- app/src/main/java/com/example/bluetooth_chat_system/DeviceListActivity.java
+++ app/src/main/java/com/example/bluetooth_chat_system/DeviceListActivity.java
@@ -2,13 +2,44 @@ package com.example.bluetooth_chat_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Set;
 
 public class DeviceListActivity extends AppCompatActivity {
-
+    private ListView listPairedDevices,listAvailableDevices;
+    private ArrayAdapter<String> adapterPairedDevices, adapterAvailableDevices;
+    private BluetoothAdapter bluetoothAdapter;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
+        context=this;
+        init();
+    }
+    private void init(){
+        listPairedDevices=findViewById(R.id.list_paired_devices);
+        listAvailableDevices=findViewById(R.id.list_available_devices);
+
+        adapterPairedDevices=new ArrayAdapter<String>(context,R.layout.device_list_item);
+        adapterAvailableDevices=new ArrayAdapter<String>(context,R.layout.device_list_item);
+
+        listPairedDevices.setAdapter(adapterPairedDevices);
+        listAvailableDevices.setAdapter(adapterAvailableDevices);
+
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices=bluetoothAdapter.getBondedDevices();
+
+        if(pairedDevices!=null && pairedDevices.size()>0){
+            for(BluetoothDevice device : pairedDevices){
+                adapterPairedDevices.add(device.getName()+"\n"+device.getAddress());
+            }
+        }
     }
 }
